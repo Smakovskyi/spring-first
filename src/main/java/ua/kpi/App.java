0743f29d2e9e8e6fa280d9b211fc6f6611bac0ff
d.smakovskiy <smakovskiy@ukr.net>
2020-04-09 10:14:30 +0300

    The first hello Spring application is working

--- src/main/java/ua/kpi/App.java
+++ src/main/java/ua/kpi/App.java
@@ -1,5 +1,9 @@
 package ua.kpi;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ua.kpi.controller.Controller;
+
 /**
  * Hello world!
  */
@@ -7,5 +11,10 @@ public class App {
 
   public static void main(String[] args) {
     System.out.println("Hello World!");
+    ApplicationContext ctx = new ClassPathXmlApplicationContext("context.xml");
+    //View view = ctx.getBean("view", View.class);
+    Controller controller = ctx
+        .getBean("controller", Controller.class);//new Controller( System.in , view );
+    controller.processUser();
   }
 }
---
+++ src/main/java/ua/kpi/view/View.java
@@ -0,0 +1,8 @@
+package ua.kpi.view;
+
+public class View {
+
+  public void print(String value) {
+    System.out.println(value);
+  }
+}
